0e67ec362f832188141af5d3a7b20795206bb41d
Ludovico Orlando Russo <ludus.russo@gmail.com>
2018-03-02 14:42:06 +0100

    up entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS2_DISTRO/setup.bash"
+source /opt/ros/$ROS2_DISTRO/share/ros2cli/environment/ros2-argcomplete.bash
 source "/ament_ws/install/setup.bash"
 exec "$@"
